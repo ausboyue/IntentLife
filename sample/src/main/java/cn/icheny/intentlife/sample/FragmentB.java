065f98f0e75f2096f18d0be76d3b10b46fa48bfc
ausboyue <ausboyue@gmail.com>
2019-06-22 16:09:59 +0800

    feat(module'sample') finish new demo with new feature
    Finish it.

--- sample/src/main/java/cn/icheny/intentlife/sample/FragmentB.java
+++ sample/src/main/java/cn/icheny/intentlife/sample/FragmentB.java
@@ -9,16 +9,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import cn.icheny.intentlife.IntentLife;
+import cn.icheny.intentlife.annotation.BindIntentKey;
+
 
 public class FragmentB extends Fragment {
-
+    @BindIntentKey("key_user")
     User mUser;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //  IntentLife inject
+        IntentLife.bind(this);
+
         View rootView = inflater.inflate(R.layout.frag_b_layout, container, false);
         final TextView tv_user_desc = rootView.findViewById(R.id.tv_user_desc);
+        tv_user_desc.setText("Hello , I am " + mUser.getName()
+                + ".\nMy job is " + mUser.getJob() + ".");
         return rootView;
     }
 
