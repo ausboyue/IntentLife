16730e55fb6ba67e0330ece2cd323cbca692b167
ausboyue <ausboyue@gmail.com>
2019-06-24 18:01:57 +0800

    fix(module'intentlife'): fix about control debuging.
    Fix bug in previous version control debugging.

--- intentlife/src/main/java/cn/icheny/intentlife/IntentLife.java
+++ intentlife/src/main/java/cn/icheny/intentlife/IntentLife.java
@@ -2,24 +2,32 @@ package cn.icheny.intentlife;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-
-import cn.icheny.intentlife.annotations.BuildConfig;
 
 /**
  * <pre>
  *     @author : www.icheny.cn
  *     @e-mail : ausboyue@gmail.com
  *     @time   : 2019.06.15
- *     @desc   : Creator.
+ *     @desc   : Use this class to simplify finding data form Intent or Bundle by binding them
+ *               with annotations.
  * </pre>
  */
 public class IntentLife {
     private static final String TAG = "IntentLife";
-    private static boolean debug = BuildConfig.DEBUG;
+    private static boolean loggable;
+
+    /**
+     * Control whether debug logging is enabled.
+     */
+    public static void setLoggable(boolean loggable) {
+        IntentLife.loggable = loggable;
+    }
 
     /**
      * BindIntentKey annotated fields in the specified {@link Activity}. The current intent
@@ -27,9 +35,12 @@ public class IntentLife {
      *
      * @param target Target activity for data binding.
      */
-    public static void bind(Activity target) {
+    public static void bind(@NonNull Activity target) {
         final Bundle sourceBundle = target.getIntent().getExtras();
-        bind(target, sourceBundle);
+        if (sourceBundle == null) {
+            return;
+        }
+        createBinder(target, sourceBundle);
     }
 
     /**
@@ -38,8 +49,12 @@ public class IntentLife {
      *
      * @param target Target fragment for data binding.
      */
-    public static void bind(android.app.Fragment target) {
-        bind(target, target.getArguments());
+    public static void bind(@NonNull android.app.Fragment target) {
+        Bundle sourceBundle = target.getArguments();
+        if (sourceBundle == null) {
+            return;
+        }
+        createBinder(target, sourceBundle);
     }
 
     /**
@@ -48,8 +63,12 @@ public class IntentLife {
      *
      * @param target Target fragment for data binding.
      */
-    public static void bind(Fragment target) {
-        bind(target, target.getArguments());
+    public static void bind(@NonNull Fragment target) {
+        Bundle sourceBundle = target.getArguments();
+        if (sourceBundle == null) {
+            return;
+        }
+        createBinder(target, sourceBundle);
     }
 
     /**
@@ -59,20 +78,28 @@ public class IntentLife {
      * @param target Target class for data binding.
      * @param source Data on which key will be looked up.
      */
-    public static void bind(Object target, Bundle source) {
+    public static void bind(@NonNull Object target, @NonNull Bundle source) {
+        createBinder(target, source);
+    }
+
+    /**
+     * Create binder.
+     */
+    private static void createBinder(@NonNull Object target, @NonNull Bundle source) {
         final String name = target.getClass().getName();
         final String binderName = name + "_Binder";
         try {
-            Class<?> clazz = Class.forName(binderName);
-            clazz.getConstructor(target.getClass(), source.getClass()).newInstance(target, source);
+            final Class<?> clazz = Class.forName(binderName);
+            final Constructor<?> constructor = clazz.getConstructor(target.getClass(), source.getClass());
+            constructor.newInstance(target, source);
         } catch (ClassNotFoundException e) {
-            if (debug) Log.d(TAG, e.getMessage());
+            if (loggable) Log.d(TAG, e.getMessage());
         } catch (IllegalAccessException e) {
-            throw new RuntimeException("Unable to invoke newInstance (target, source)", e);
+            throw new RuntimeException("Unable to invoke newInstance(target, source)", e);
         } catch (InstantiationException e) {
-            throw new RuntimeException("Unable to invoke newInstance (target, source)", e);
+            throw new RuntimeException("Unable to invoke newInstance(target, source)", e);
         } catch (NoSuchMethodException e) {
-            throw new RuntimeException("Unable to find binding constructor for " + binderName, e);
+            throw new RuntimeException("Unable to find binder constructor for " + binderName, e);
         } catch (InvocationTargetException e) {
             Throwable cause = e.getCause();
             if (cause instanceof RuntimeException) {
@@ -81,7 +108,7 @@ public class IntentLife {
             if (cause instanceof Error) {
                 throw (Error) cause;
             }
-            throw new RuntimeException("Unable to create binding instance.", cause);
+            throw new RuntimeException("Unable to create binder instance.", cause);
         }
     }
 }
