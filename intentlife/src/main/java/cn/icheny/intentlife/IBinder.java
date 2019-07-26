4600779816c09d42dde66432cf74c5ac3f974130
ausboyue <ausboyue@gmail.com>
2019-07-26 17:59:02 +0800

    Modify super interface IBinder.

--- intentlife/src/main/java/cn/icheny/intentlife/IBinder.java
+++ intentlife/src/main/java/cn/icheny/intentlife/IBinder.java
@@ -1,16 +1,13 @@
 package cn.icheny.intentlife;
 
-import android.support.annotation.UiThread;
 
 /**
  * <pre>
  *     @author : www.icheny.cn
  *     @e-mail : ausboyue@gmail.com
  *     @time   : 2019.07.25
- *     @desc   : The binder interface.All of its child will overide {@link #bind()} function.
+ *     @desc   : This is super interface for all binder.
  * </pre>
  */
 public interface IBinder {
-    @UiThread
-    void bind();
 }
